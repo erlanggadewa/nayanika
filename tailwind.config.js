c166daef347ca7efb5b38bb85dc3d1c67b4982f3
ErlanggaDewa <erlanggadewa7@gmail.com>
2021-10-04 11:58:50 +0700

    update before using JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  mode: "jit",
+  purge: ["./views/layouts/*.ejs", "./views/*.ejs", "./views/partials/*.ejs"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { inset: { "1/5": "20%" } },
   },
   variants: {
-    extend: {},
+    extend: {
+      width: ["hover"],
+      visibility: ["hover", "focus", "responsive", "group-hover"],
+      padding: ["hover", "focus", "responsive", "group-hover"],
+      display: ["hover", "focus", "responsive", "group-hover"],
+      margin: ["hover", "focus", "responsive", "group-hover"],
+    },
   },
   plugins: [],
 };
