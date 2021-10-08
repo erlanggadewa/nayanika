e716613cc33ff5a2a0eb1688954eb78fbda2c57c
ErlanggaDewa <erlanggadewa7@gmail.com>
2021-10-08 10:00:39 +0700

    budaya

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,20 @@ module.exports = {
   purge: ["./views/layouts/*.ejs", "./views/*.ejs", "./views/partials/*.ejs"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: { inset: { "1/5": "20%" } },
+    extend: {
+      inset: { "1/5": "20%" },
+      left: { "4/5": "80%" },
+      right: { "1/5": "20%" },
+    },
   },
   variants: {
     extend: {
-      width: ["hover"],
-      visibility: ["hover", "focus", "responsive", "group-hover"],
-      padding: ["hover", "focus", "responsive", "group-hover"],
-      display: ["hover", "focus", "responsive", "group-hover"],
-      margin: ["hover", "focus", "responsive", "group-hover"],
+      // width: ["hover"],
+      // visibility: ["hover", "focus", "responsive", "group-hover"],
+      // padding: ["hover", "focus", "responsive", "group-hover"],
+      // display: ["hover", "focus", "responsive", "group-hover"],
+      // margin: ["hover", "focus", "responsive", "group-hover"],
+      // transitionDuration: ["hover", "focus", "responsive", "group-hover"],
     },
   },
   plugins: [],
